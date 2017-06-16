e9ef8b45b346d8bd2da6644aac1c30b7b93082c3
byamba3 <byamba3@yahoo.com>
2017-06-16 01:42:38 -0500

    Movie Project

--- entertainment_center.py
+++ entertainment_center.py
@@ -4,7 +4,7 @@ import urllib
 import json
 import ssl
 
-#Pre-defined Movie objects for non API option
+# Pre-defined Movie objects for non API option
 
 logan = media.Movie("Logan",
                     "In the near future, a weary Logan (Hugh Jackman) cares for an ailing Professor X (Patrick Stewart) at a remote outpost on the Mexican border. His plan to hide from the outside world gets upended when he meets a young mutant (Dafne Keen) who is very much like him. Logan must now protect the girl and battle the dark forces that want to capture her.",
@@ -45,7 +45,7 @@ ex_machina = media.Movie("Ex Machina",
                          "Caleb Smith (Domhnall Gleeson) a programmer at a huge Internet company, wins a contest that enables him to spend a week at the private estate of Nathan Bateman (Oscar Isaac), his firm's brilliant CEO. When he arrives, Caleb learns that he has been chosen to be the human component in a Turing test to determine the capabilities and consciousness of Ava (Alicia Vikander), a beautiful robot. However, it soon becomes evident that Ava is far more self-aware and deceptive than either man imagined.",
                          "http://t3.gstatic.com/images?q=tbn:ANd9GcQe8L-1PTMlUf-si2Oy6BTd9ZtbWH7BSRSF5k5JGNATxOHzyIdg",
                          "https://www.youtube.com/watch?v=3hAlv0xLJJ4")
-                        
+
 the_martian = media.Movie("The Martian",
                           "When astronauts blast off from the planet Mars, they leave behind Mark Watney (Matt Damon), presumed dead after a fierce storm. With only a meager amount of supplies, the stranded visitor must utilize his wits and spirit to find a way to survive on the hostile planet. Meanwhile, back on Earth, members of NASA and a team of international scientists work tirelessly to bring him home, while his crew mates hatch their own plan for a daring rescue mission.",
                           "http://t2.gstatic.com/images?q=tbn:ANd9GcTkKPZ7EIOafEsemyn6zTIDeGYthKC_Okgxi1eX6diuOT3xKWXQ",
@@ -65,10 +65,13 @@ inside_out = media.Movie("Inside Out",
                          "Riley (Kaitlyn Dias) is a happy, hockey-loving 11-year-old Midwestern girl, but her world turns upside-down when she and her parents move to San Francisco. Riley's emotions -- led by Joy (Amy Poehler) -- try to guide her through this difficult, life-changing event. However, the stress of the move brings Sadness (Phyllis Smith) to the forefront. When Joy and Sadness are inadvertently swept into the far reaches of Riley's mind, the only emotions left in Headquarters are Anger, Fear and Disgust.",
                          "http://t0.gstatic.com/images?q=tbn:ANd9GcTtZdvrahQxfjGkSBJCS-uiZKUfJNH4ddsqgCbV5oFkQiQ-tszE",
                          "https://www.youtube.com/watch?v=YEk_5aT5Gng")
-# Add your pre-defined movies here: 
-predefined_movies = [logan, wonder_woman, kubo, your_name, finding_dory, zootopia, moana, ex_machina, the_martian, mad_max, gotg, inside_out]
+# Add your pre-defined movies here:
+predefined_movies = [logan, wonder_woman, kubo, your_name, finding_dory,
+                     zootopia, moana, ex_machina, the_martian, mad_max, gotg, inside_out]
 
 # Returns a json data from a URL
+
+
 def get_json_from_url(url):
     context = ssl._create_unverified_context()
     response = urllib.urlopen(url, context=context)
@@ -76,35 +79,42 @@ def get_json_from_url(url):
     return data
 
 # Takes in a link and the api, then returns a movie array from the json
+
+
 def get_movies_from_json(link, api):
     url = link + api
     json_movie_collection = get_json_from_url(url)
-    
+
     movies = []
-    
+
     for movie in json_movie_collection['results']:
         poster_url = "http://image.tmdb.org/t/p/w500//" + movie['poster_path']
 
         # Gets an additional JSON just for the trailer
-        url_video = "http://api.themoviedb.org/3/movie/" + str(movie['id']) + "/videos?api_key=" + api
+        url_video = "http://api.themoviedb.org/3/movie/" + \
+            str(movie['id']) + "/videos?api_key=" + api
         movie_video = get_json_from_url(url_video)
-        trailer_url = "https://www.youtube.com/watch?v=" + movie_video['results'][0]['key']
-    
-        temp_movie = media.Movie(movie['title'], movie['overview'], poster_url, trailer_url)
+        trailer_url = "https://www.youtube.com/watch?v=" + \
+            movie_video['results'][0]['key']
+
+        temp_movie = media.Movie(
+            movie['title'], movie['overview'], poster_url, trailer_url)
         movies.append(temp_movie)
 
     return movies
 
-#Main
+
+# Main
 print("Movie Website - Byamba3")
 is_valid = False
-api_base_url= "https://api.themoviedb.org/3/movie/popular?api_key="
+api_base_url = "https://api.themoviedb.org/3/movie/popular?api_key="
 api_key = "8e0615057ed71d85a782edf61f3a1a13"
 
 all_movies = []
 
 while is_valid == False:
-    input_value = raw_input('Do you want to use the API version? Enter Y or N: ')
+    input_value = raw_input(
+        'Do you want to use the API version? Enter Y or N: ')
     if str.lower(input_value) == 'y' or str.lower(input_value) == 'yes':
         print("Fetching live from MovieDB...")
         all_movies = get_movies_from_json(api_base_url, api_key)
@@ -115,7 +125,3 @@ while is_valid == False:
         is_valid = True
 
 fresh_tomatoes.open_movies_page(all_movies)
-
-
-
-
