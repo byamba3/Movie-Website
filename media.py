cbe7ab8e7120bbfbe1a2a1ebf68690c368f780a3
byamba3 <byamba3@yahoo.com>
2017-06-16 11:35:12 -0500

    Movie Project - Revised

--- media.py
+++ media.py
@@ -1,13 +1,16 @@
 import webbrowser
 
+
 class Movie():
-    
-    #Constructs a Movie object with title, storyline, poster image URL, and trailer URL
+
+    # Constructs a Movie object with title, poster image URL, and trailer URL
+
     def __init__(self, movie_title, poster_image, trailer_youtube):
         self.title = movie_title
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
 
-    #Opens the movie's trailer in a web browser
+    # Opens the movie's trailer in a web browser
+
     def show_trailer(self):
         webbrowser.open(self.trailer_youtube_url)
